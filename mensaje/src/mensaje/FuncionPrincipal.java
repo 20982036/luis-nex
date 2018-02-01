e10df0d271fe54b7ef4939cd2cb4fb87e9b55f44
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-02-01 01:21:07 -0600

    Compilando
    se creo Una nueva clase java y se corrigieron los errores de compilacion

---
+++ mensaje/src/mensaje/FuncionPrincipal.java
@@ -0,0 +1,17 @@
+
+package mensaje;
+
+/**
+ *
+ * @author Lenovo-pc
+ */
+public class FuncionPrincipal {
+    
+public static void main (String arg[]){
+    
+Mensaje miMensaje = new Mensaje ( );
+miMensaje.imprime ( );
+
+}
+
+}
--- mensaje/src/mensaje/Mensaje.java
+++ mensaje/src/mensaje/Mensaje.java
@@ -1,30 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mensaje;
 
-/**
- *
- * @author moya
- */
 public class Mensaje {
 
     private String frase;
 
 public void imprime ( ) {
     
-String frase = “Este es mi mensaje”;
-System.out.println("mensaje.Mensaje.imprime()" + frase);
-System.out.println(“La frase del mensaje es: ” + frase );
+frase = "Este es mi mensaje";
+System.out.println("La frase del mensaje es: " + frase );
+  }
 }
 
-/*
-}public class FuncionPrincipal {
-public static void main (String arg[]){
-Mensaje miMensaje = new Mensaje ( );
-miMensaje.imprime ( );
-}*/
 
-}
+
+
