1ba039b7179c91cf58f7b38116ff9282eaf09f12
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-02-01 01:25:41 -0600

    comentarios
    Anoto comentarios para hacer mas facil la lectura del mismo

--- mensaje/src/mensaje/FuncionPrincipal.java
+++ mensaje/src/mensaje/FuncionPrincipal.java
@@ -7,11 +7,14 @@ package mensaje;
  */
 public class FuncionPrincipal {
     
+                /*Este es el metodo MAIN   siempre que compiles algo
+                * Tomara de referencia el programa el metodo MAIN
+                *  y se ejecutara
+                */
 public static void main (String arg[]){
     
 Mensaje miMensaje = new Mensaje ( );
 miMensaje.imprime ( );
 
-}
-
+  }
 }
