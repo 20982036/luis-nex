61c15b3057ad0a846c03feb2ba0a2ac84d5af469
20982036 <36024477+20982036@users.noreply.github.com>
2018-02-02 23:31:58 -0600

    multiplicación
    proyecto nuevo de multiplicación

--- tabla(x)/multiplicaciones.java
+++ tabla(x)/multiplicaciones.java
@@ -1,4 +1,44 @@
+package tabla;
+
+import java.util.Scanner;
+
+class multiplicaciones {
+    
 
 public class multiplicaciones {
-    
+    import java.io.*;
+class DatosEntrada {
+public static String cadena( ) {
+try{ BufferedReader leeDato=new BufferedReader(new InputStreamReader(System.in));
+return leeDato.readLine();
+} catch(IOException e) {
+return " ";
+}
+}
+public static int entero( ) {
+try{ return Integer.parseInt(cad());
+} catch(NumberFormatException e) {
+return -1;
+}
+}
+public static double real( ){
+try{ return Double.parseDouble(cad());
+} catch(NumberFormatException e) {
+return -1.0;
+}
+}
+public static float flotante(){
+try { return Float.parseFloat(cad());
+} catch (NumberFormatException e) {
+return -1.0f;
+}
+}
+public static char caracter(){
+try{ return cad().charAt(0);
+} catch(IndexOutOfBoundsException e) {
+return ' ';
+}
+}
+}
+}
 }
