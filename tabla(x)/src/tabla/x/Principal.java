bb8a2fba3418c24a36b2fa4c0cf525e5f4fe31b5
20982036 <36024477+20982036@users.noreply.github.com>
2018-02-02 23:01:48 -0600

    Tabla X
    Tabla X

---
+++ tabla(x)/src/tabla/x/Principal.java
@@ -0,0 +1,18 @@
+class Tabla {
+private int numTabla;
+private int limite;
+public void leeNums( ) {
+System.out.print ("Que tabla quieres multiplicar: ");
+numTabla = DatosEntrada.entero( );
+System.out.print ("Cuantas veces?: ");
+limite = DatosEntrada.entero( );
+}
+public void multTabla( ) {
+if ( numTabla == -1 || limite == -1 )
+System.out.println("Error: el valor escrito es incorrecto");
+else {
+for (int i=1; i<=limite; i++)
+System.out.println(numTabla + "x" + i + "=" + (numTabla*i));
+}
+}
+}
--- tabla/src/tabla/Principal.java
+++ tabla/src/tabla/Principal.java
@@ -3,7 +3,7 @@ package tabla;
 import java.util.Scanner;
 
 class Principal { 
-    public static void main(String ar [] ) { 
+    public static void main(String ar [] ) {   //El metodo main es la funcion principal  o la primera que se ejecutara
         
         Scanner leeDato = new Scanner(System.in); 
         System.out.print ("Que tabla quieres multiplicar: "); 
