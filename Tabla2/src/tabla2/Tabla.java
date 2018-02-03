0d62710cf0fc557626cde41b87a6a9902fc8a4cc
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-02-03 00:37:41 -0600

    cambios tabla2
    se anexaron nuevos datos

--- Tabla2/src/tabla2/Tabla.java
+++ Tabla2/src/tabla2/Tabla.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tabla2;
 
 class Tabla{
@@ -12,7 +7,7 @@ private int limite;
     
 public void  leeNums( ) {
 System.out.print ("Que tabla quieres multiplicar: ");
-numTabla = DatosEntrada.entero( );
+numTabla = DatosEntrada.entero();
 System.out.print ("Cuantas veces?: ");
 limite = DatosEntrada.entero( );
 }
