89b6af9409119aef51b8e63ad1f554a4af317680
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-02-03 00:16:22 -0600

    Tabla2 Proyect
    Te mando el nuevo proyecto de tabla

---
+++ Tabla2/src/tabla2/Principal.java
@@ -0,0 +1,12 @@
+
+package tabla2;
+
+class Principal { 
+    
+    public static void main(String ar [] ) { 
+        
+        Tabla miTabla = new Tabla( ); 
+        miTabla.leeNums( ); 
+        miTabla.multTabla( );
+    }
+}
---
+++ Tabla2/src/tabla2/Tabla.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabla2;
+
+class Tabla{
+    
+private int numTabla;
+private int limite;
+    
+public void  leeNums( ) {
+System.out.print ("Que tabla quieres multiplicar: ");
+numTabla = DatosEntrada.entero( );
+System.out.print ("Cuantas veces?: ");
+limite = DatosEntrada.entero( );
+}
+
+
+public void multTabla( ) {
+    if ( numTabla == -1 || limite == -1 )
+System.out.println("Error: el valor escrito es incorrecto");
+    else {
+for (int i=1; i<=limite; i++)
+System.out.println(numTabla + "x" + i + "=" + (numTabla*i));
+      }
+   }
+ }
