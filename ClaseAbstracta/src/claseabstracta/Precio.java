41cbca1bce413b0b4d604965c36b22ad51555503
20982036 <36024477+20982036@users.noreply.github.com>
2018-02-18 22:08:34 -0600

    Metodos Abstractos
    Update

--- ClaseAbstracta/src/claseabstracta/Precio.java
+++ ClaseAbstracta/src/claseabstracta/Precio.java
@@ -32,3 +32,43 @@ public abstract class Camion extends autopista{
         System.out.printIn("La cuota a pagar sera de $125 + 15.50 por tonelada + 16 de IVA");
     }
 }
+
+
+//*la opcion "tipo" será el tipo de transporte que pase por la autopista*
+
+
+public DetallePrecio(int tipo, String sincasco, String tonelada, double precio) throws IOException{
+        autopista(codigo, descripcion, perecedero, cantidad, precio);
+        this.iva = iva;
+        this.subtotal = subtotal;
+        this.total = total;
+        this.aumento = aumento;
+        
+        iva = precio * 0.16;
+        subtotal = precio*cantidad;
+        total = iva+subtotal;
+       
+    }
+
+
+
+public abstract void imprimirDatos() extends throws IOException{
+        
+        char opcion;
+        Scanner leeDato = new Scanner(System.in);
+        System.out.println("Total de importe: "+total);
+        System.out.println("Total de IVA: "+iva);
+        System.out.println("Subtotal sobre el importe: "+subtotal);
+        System.out.println("Total del importe con cargos extra: "+descuento);
+        System.out.println("¿Desea regresar al menu principal? (S|N): ");
+        opcion = leeDato.next().charAt(0);
+        if(opcion == 'S'||opcion=='s'){
+          
+            Menu menu = new Menu();
+            menu.menu();
+        }    
+        else{
+            
+            System.out.println("Gracias por su visita");
+            System.exit(0);
+        }
