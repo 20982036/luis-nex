57100dd3630c2173b407d2f442dd0ee8dd6289e3
20982036 <36024477+20982036@users.noreply.github.com>
2018-02-18 22:43:02 -0600

    new branch abstract
    update

---
+++ ClaseAbstracta/src/claseabstracta/ClaseAbstracta.java
@@ -0,0 +1,12 @@
+
+package claseabstracta;
+
+public abstract class ClaseAbstracta {
+   
+    public abstract void autopista();{
+    
+        Menu menu = new Menu();
+        menu.menu();
+    
+}
+}
