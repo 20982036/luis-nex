03e2eac322adc19975aef0fcfe49db05991d34ee
20982036 <36024477+20982036@users.noreply.github.com>
2018-02-20 22:50:18 -0600

    fist one
    implementation

--- ClaseAbstracta/src/claseabstracta/Menu.java
+++ ClaseAbstracta/src/claseabstracta/Menu.java
@@ -1,12 +1,47 @@
 
-package claseabstracta;
+package Menu;
 import java.awt.Font;
 import java.io.*;
 import java.util.Scanner;
 
-public class Menu {
+public abstract class Menu {
     public static void main(String [] args){
-        Auto auto = new auto();   
+        Auto auto = new auto();  
+        Moto moto = new moto(); 
+        Camion camion = new camion(); 
         
     }
 }
+
+public abstract detalles extends detalles{
+    private double iva;
+    private double subtotal;
+    private double total;
+    private double casco;
+    private double tonelada;
+    private String descripcion;
+}
+
+public abstract void imprimirDatos() throws IOException{
+    char opcion;
+     Scanner leeDato = new Scanner(System.in);
+     System.out.println("Total de IVA: "+iva);
+     System.out.println("Subtotal de compra: "+subtotal);
+     System.out.println("Total de compra: "+total);
+     System.out.println("Casco: "+casco);
+     System.out.println("Tonelada: "+tonelada);
+     System.out.println("Total de auto: "+auto);
+     System.out.println("Total de moto: "+moto);
+     System.out.println("Total de camion: "+camion);
+ System.out.println("¿Desea regresar al menu principal? (S|N): ");
+        opcion = leeDato.next().charAt(0);
+        if(opcion == 'S'||opcion=='s'){
+          
+            Menu menu = new Menu();
+            menu.menu();
+        }    
+        else{
+            
+            System.out.println("Vuelva pronto!");
+            System.exit(0);
+        }
--- ClaseAbstracta/src/claseabstracta/Precio.java
+++ ClaseAbstracta/src/claseabstracta/Precio.java
@@ -1,5 +1,5 @@
 
-package claseabstracta;
+package Precio;
 import java.awt.Font;
 import java.io.*;
 import java.util.Scanner;
@@ -31,4 +31,28 @@ public abstract class Camion extends autopista{
     public void cuota(){
         System.out.printIn("La cuota a pagar sera de $125 + 15.50 por tonelada + 16 de IVA");
     }
+}
+
+
+public abstract class Precio (String auto,String moto,String camion,String iva, String casco, String tonelada, String descripcion, double precio)throws IOException{
+autopista(auto, moto, camion, descripcion, precio, iva, casco, tonelada);
+this.descrpcion = descripcion;
+this.precio= precio;
+this iva = iva;
+this.casco = casco;
+this.tonelada = tonelada;
+this. total = total;
+this.subtotal = subtotal;
+
+
+iva = precio *0.16;
+subtotal = precio*cantidad;
+total = iva+subtotal;
+
+if(casco >= 's') if(casco >= 'S'){
+    suma = total - (total*.07);
+}
+
+if(tonelada >= 's') if(tonelada >= 'S'){
+    suma = total - (total*.07);
 }
